404528dbbe2905b4520a9b494ede4715a4fee864
Nathan Toombs <nathan.toombs@classyllama.com>
2022-05-23 22:51:15 -0500

    Fixing head tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,18 +18,18 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/icon-16x16.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/icon-32x32.png"
           rel="icon"
           type="image/png"
           sizes="32x32"
         />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        {/* <link rel="apple-touch-icon" href="/apple-icon.png"></link>*/}
         <meta name="theme-color" content="#317EFB" />
         <meta name='application-name' content='PWA App' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
@@ -43,30 +43,26 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name='msapplication-tap-highlight' content='no' />
         <meta name='theme-color' content='#000000' />
 
-        <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
-        <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
-        <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />
-
-        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
-        <link rel='manifest' href='/manifest.json' />
-        <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+        {/* <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />*/}
+        {/* <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />*/}
+        {/* <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />*/}
+        {/* <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />*/}
+        {/* <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />*/}
         <link rel='shortcut icon' href='/favicon.ico' />
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
 
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:url' content='https://yourdomain.com' />
-        <meta name='twitter:title' content='PWA App' />
-        <meta name='twitter:description' content='Best PWA App in the world' />
-        <meta name='twitter:image' content='https://yourdomain.com/icons/android-chrome-192x192.png' />
-        <meta name='twitter:creator' content='@DavidWShadow' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='PWA App' />
-        <meta property='og:description' content='Best PWA App in the world' />
-        <meta property='og:site_name' content='PWA App' />
-        <meta property='og:url' content='https://yourdomain.com' />
-        <meta property='og:image' content='https://yourdomain.com/icons/apple-touch-icon.png' />
+        {/* <meta name='twitter:card' content='summary' />*/}
+        {/* <meta name='twitter:url' content='https://yourdomain.com' />*/}
+        {/* <meta name='twitter:title' content='PWA App' />*/}
+        {/* <meta name='twitter:description' content='Best PWA App in the world' />*/}
+        {/* <meta name='twitter:image' content='https://yourdomain.com/icons/android-chrome-192x192.png' />*/}
+        {/* <meta name='twitter:creator' content='@DavidWShadow' />*/}
+        {/* <meta property='og:type' content='website' />*/}
+        {/* <meta property='og:title' content='PWA App' />*/}
+        {/* <meta property='og:description' content='Best PWA App in the world' />*/}
+        {/* <meta property='og:site_name' content='PWA App' />*/}
+        {/* <meta property='og:url' content='https://yourdomain.com' />*/}
+        {/* <meta property='og:image' content='https://yourdomain.com/icons/apple-touch-icon.png' />*/}
       </Head>
       <Component {...pageProps} />
     </>
